e76663cf768ec26368265296e814d5530067f159
recursivetechsolution-web <recursivetechsolution@gmail.com>
2026-09-18 01:15:50 +0000

    firstCommit

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const siteNav = document.querySelector('.site-nav');
+
+menuToggle.addEventListener('click', () => {
+  const isOpen = siteNav.classList.toggle('open');
+  menuToggle.setAttribute('aria-expanded', String(isOpen));
+});
+
+siteNav.querySelectorAll('a').forEach((link) => {
+  link.addEventListener('click', () => {
+    siteNav.classList.remove('open');
+    menuToggle.setAttribute('aria-expanded', 'false');
+  });
+});
